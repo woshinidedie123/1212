a9d6b4bed3e8b9486cd385b55decde52047a6b91
woshinidedie123 <1172121865@qq.com>
2021-10-14 21:03:15 +0800

    666

---
+++ src/第二章编程题/cxqd1.java
@@ -0,0 +1,10 @@
+package 第二章编程题;
+
+public class cxqd1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/第二章编程题/cxqd2.java
@@ -0,0 +1,10 @@
+package 第二章编程题;
+
+public class cxqd2 {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
